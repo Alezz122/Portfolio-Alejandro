bc12f4610da9f0a1164cb452aa6dcb99f537139f
Alejandro <alezbussines@gmail.com>
2022-11-10 12:18:47 +0000

    Portfolio 0.6.0

---
+++ Pagina Web Portfolio/js/index.js
@@ -0,0 +1,16 @@
+
+
+var textAnim = document.getElementById('text-anim');
+
+
+const textsToType = ['I build nice websites', 'I design nice products'];
+const speed = 30;
+
+var typed = new Typed('#text-anim', {
+    strings: textsToType,
+    typeSpeed: speed,
+    showCursor: true,
+    cursorChar: '|',
+    startDelay: 200,
+    loop: true
+});
