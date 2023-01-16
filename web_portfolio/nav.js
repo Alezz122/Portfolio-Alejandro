71aa259a5d2d71edd3050ca8568966b893ed7f12
Alejandro <alezbussines@gmail.com>
2023-01-16 14:02:08 +0000

    Delete web_portfolio directory

--- web_portfolio/nav.js
+++
@@ -1,7 +0,0 @@
-const btnNav = document.getElementById('btn-display-nav'),
-navEl = document.querySelector('nav');
-
-
-btnNav.addEventListener('click', () => {
-    navEl.classList.toggle('nav-display');
-});
